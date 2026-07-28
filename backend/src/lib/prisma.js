617f9acb93f794c7e845f9620e075169c62b5d7a
SAM-condinng <kamunyagachuru@gmail.com>
2026-07-28 12:36:12 +0300

    Modified patches

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,15 +1,20 @@
 import "dotenv/config";
+import pg from "pg";
 import { PrismaClient } from "../../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis;
 
-const adapter = new PrismaPg({
+const pool = globalForPrisma.pgPool || new pg.Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+const adapter = new PrismaPg(pool);
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
+  globalForPrisma.pgPool = pool;
 }
+
